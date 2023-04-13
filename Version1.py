3fd0be8ce1feb0cfdca2dd42845dabdd08bed621
CrystalTea <130581824+CrystallTea@users.noreply.github.com>
2023-04-13 21:10:57 +0000

    Add files via upload

---
+++ Version1.py
@@ -0,0 +1,14 @@
+line1 = "Quack Quack"
+print(line1)
+
+line2 = "How's there?"
+print(line2)
+
+line3 = "Quack?"
+print(line3)
+
+line4 = "*door opening sound..*"
+print(line4)
+
+line5 = "Oh.. no one is here...wait..Damnit! the secret word! ughh those deliveries are getting funnier"
+print(line5)

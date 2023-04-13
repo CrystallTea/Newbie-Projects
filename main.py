97096dac2c2d6542dd85ddef686bcc059e8919f5
CrystalTea <130581824+CrystallTea@users.noreply.github.com>
2023-04-13 21:06:23 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+Line1 = "-Quack Quack"
+Line2 = "-How's there?"
+print(Line1 + '\n' + Line2)
+
+Line3 = "-Quack"
+Line4 = "*door's opening sound..*"
+Line5 = "Oh.. no one is here...wait..Damnit! the secret word! ughh those deliveries are getting funnier"
+print(Line3 + '\n' + '\n' + Line4 + '\n' + '\n' + Line5)

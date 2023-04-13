9dfdd8ee8541dec38a4cdb3ed40789e4f2634bf4
CrystalTea <130581824+CrystallTea@users.noreply.github.com>
2023-04-13 21:24:43 +0000

    Version2
    Reduced the number of lines in the code, used print ("" + '\n' + "") to customize the spacing between lines.

--- Version1.py
+++ Version1.py
@@ -1,14 +1,8 @@
-line1 = "Quack Quack"
-print(line1)
+Line1 = "-Quack Quack"
+Line2 = "-How's there?"
+print(Line1 + '\n' + Line2)
 
-line2 = "How's there?"
-print(line2)
-
-line3 = "Quack?"
-print(line3)
-
-line4 = "*door opening sound..*"
-print(line4)
-
-line5 = "Oh.. no one is here...wait..Damnit! the secret word! ughh those deliveries are getting funnier"
-print(line5)
+Line3 = "-Quack"
+Line4 = "*door's opening sound..*"
+Line5 = "Oh.. no one is here...wait..Damnit! the secret word! ughh those deliveries are getting funnier"
+print(Line3 + '\n' + '\n' + Line4 + '\n' + '\n' + Line5)
